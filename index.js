9fe29d91894d9cfc84daf2dc0da2f07cfa184579
unknown <r.tomaszewski@gmail.com>
2020-09-29 16:58:14 +0200

    Remove some comments

--- index.js
+++ index.js
@@ -16,40 +16,8 @@ app.get('/', (req, res) => {
         return console.log("Nieprawidłowe dane", err)
       } else {
         let items = result.rss.channel[0].item;
-
-        for (let i = 0; i < items.length; i++) {
-          items[i].availability = items[i]['g:availability']
-          delete items[i]["g:availability"];
-
-          items[i].gtin = items[i]['g:gtin']
-          delete items[i]["g:gtin"];
-
-          items[i].id = items[i]['g:id']
-          delete items[i]["g:id"];
-
-          items[i].image_link = items[i]['g:image_link']
-          delete items[i]["g:image_link"];
-
-          items[i].item_group_id = items[i]['g:item_group_id']
-          delete items[i]["g:item_group_id"];
-
-          items[i].price = items[i]['g:price']
-          delete items[i]["g:price"];
-
-          items[i].product_type = items[i]['g:product_type']
-          delete items[i]["g:product_type"];
-
-          items[i].quantity = items[i]['g:quantity']
-          delete items[i]["g:quantity"];
-
-          items[i].shipping_weight = items[i]['g:shipping_weight']
-          delete items[i]["g:shipping_weight"];
-
-          items[i].size = ['g:size']
-          delete items[i]["g:size"];
-
-          delete items[i]["g:color"];
-        }
+        let names = [];
+        let urls = [];
 
         for (let i = 0; i < items.length; i++) {
           items[i].Podatek_Vat = "23"
@@ -80,56 +48,54 @@ app.get('/', (req, res) => {
           delete items[i].description;
 
           items[i].Dostepnosc = {
-            __cdata: [items[i]['availability'][0]]
+            __cdata: [items[i]['g:availability'][0]]
           };
-          delete items[i].availability;
+          delete items[i]['g:availability'];
 
           items[i].Kod_ean = {
-            __cdata: [items[i]['gtin'][0]]
+            __cdata: [items[i]['g:gtin'][0]]
           }
-          delete items[i].gtin;
+          delete items[i]['g:gtin'];
 
           items[i].Nr_katalogowy = {
-            __cdata: [items[i]['id'][0]]
+            __cdata: [items[i]['g:id'][0]]
           }
-          delete items[i].id;
+          delete items[i]['g:id'];
 
-          if (items[i]['image_link'] === undefined) {
-            items[i].Zdjecie_glowne = []
+          if (items[i]['g:image_link'] === undefined) {
+            console.log("Element: g:image_link nie istnieje w produkcie " + (i + 1))
           } else {
-            items[i].Zdjecie_glowne = items[i]['image_link'][0];
+            items[i].Zdjecie_glowne = items[i]['g:image_link'][0];
           }
+          delete items[i]['g:image_link'];
 
-          delete items[i].image_link;
+          delete items[i]['g:item_group_id']
 
-          delete items[i].item_group_id
-
-          items[i].Cena_brutto = items[i]['price'][0];
-          delete items[i].price;
+          items[i].Cena_brutto = items[i]['g:price'][0];
+          delete items[i]['g:price'];
           items[i].Cena_brutto = items[i].Cena_brutto.substring(0, items[i].Cena_brutto.length - 7)
 
-          items[i].Ilosc_produktow = Number(items[i]['quantity'][0]).toFixed(2);
-          delete items[i].quantity;
+          items[i].Ilosc_produktow = Number(items[i]['g:quantity'][0]).toFixed(2);
+          delete items[i]['g:quantity'];
 
-          if (items[i].product_type === undefined) {
+          if (items[i]['g:product_type'] === undefined) {
             items[i].Kategoria = {
               __cdata: ["KELLYS/NIESKLASYFIKOWANE"]
             };
           } else {
             items[i].Kategoria = {
-              __cdata: [items[i]['product_type'][0].replace(/ /g, '').replace(/>/g, '/').replace(/\n/g, '')]
+              __cdata: [items[i]['g:product_type'][0].replace(/(?:\\[rn])+/g, "").replace(/ > /g, '/').trim()]
             };
           }
-          delete items[i].product_type;
+          delete items[i]['g:product_type'];
 
-          items[i].Waga = items[i]['shipping_weight'][0];
+          items[i].Waga = items[i]['g:shipping_weight'][0];
           items[i].Waga = Number(items[i].Waga.substring(0, items[i].Waga.length - 7)) + 6
-          delete items[i].shipping_weight;
+          delete items[i]['g:shipping_weight'];
 
           items[i].Nazwa_produktu = {
-            __cdata: [items[i]['title'][0]]
+            __cdata: [items[i]['title'][0].replace(/(?:\\[rn])+/g, "").trim()]
           }
-          items[i].Nazwa_produktu.__cdata[0] = items[i].Nazwa_produktu.__cdata[0].substring(3, items[i].Nazwa_produktu.__cdata[0].length - 3)
           delete items[i].title;
 
           items[i].Meta_tytul = {
@@ -148,41 +114,33 @@ app.get('/', (req, res) => {
             __cdata: ["kellysbike, kellys 2021, kellys rowery, rowery kellys, rowery górskie, rowery elektryczne, rowery"]
           }
 
-          delete items[i].size;
+          delete items[i]['g:size'];
 
-          delete items[i].color;
+          delete items[i]["g:color"];
+
+          //--------------------------------------------------------------------------------------------//
           /*
           Linijka kodu poniżej służy do usuwania kategorii. Odkomentowywać ją tylko jeśli potrzebny jest plik
           tylko do aktualizacji. Do wystawiania produktów powinna być zakomentowana.
           */
-          delete items[i].Kategoria
-          //console.log(items[i].Opis)
-        }
+          //delete items[i].Kategoria
+          //--------------------------------------------------------------------------------------------//
 
-        let names = [];
-        let urls = [];
-        for (let i = 0; i < items.length; i++) {
           names.push(items[i].Nazwa_produktu.__cdata[0])
           urls.push(items[i].Zdjecie_glowne)
-        }
 
-        for (let i = 0; i < items.length; i++) {
           for (let k = 0; k < urls.length; k++) {
             if (i === k) {
               items[i].Opis.__cdata.unshift(`<a href="/images/${urls[k] !== undefined ? urls[k].slice(29) : ""}" target="_blank"><img alt="alt" src="/images/${urls[k] !== undefined ? urls[k].slice(29) : ""}" width = "300px" /></a > <br />`)
             }
           }
-        }
 
-        for (let i = 0; i < items.length; i++) {
           for (let k = 0; k < names.length; k++) {
             if (i === k) {
               items[i].Opis.__cdata.unshift(`<b>${names[i]}</b><br />`)
             }
           }
-        }
 
-        for (let i = 0; i < items.length; i++) {
           let desc1 = items[i].Opis.__cdata[0];
           let desc2 = items[i].Opis.__cdata[1];
           let desc3 = items[i].Opis.__cdata[2];
@@ -193,9 +151,7 @@ app.get('/', (req, res) => {
           items[i].Opis = {
             __cdata: [descResult]
           };
-        }
 
-        for (let i = 0; i < items.length; i++) {
           if (Number(items[i].Ilosc_produktow) === 0) {
             items[i].Dostepnosc.__cdata[0] = "Wyprzedane"
           } else if (Number(items[i].Ilosc_produktow) > 0 && Number(items[i].Ilosc_produktow) < 6) {
@@ -217,7 +173,7 @@ app.get('/', (req, res) => {
 
         const finalXML = xmlVersion.concat(xml);
 
-        fs.writeFile('./output/converted.xml', finalXML, (err) => {
+        fs.writeFile('./output/kellysFinal.xml', finalXML, (err) => {
           if (err) throw err;
           console.log('The file has been saved!');
         });
